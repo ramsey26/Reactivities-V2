c91640ebb0669c4893667ef0b06779ca9c1c0b48
Ayush Vyas <ayush.vyas@voxco.com>
2025-06-18 15:27:07 +0530

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -10,12 +10,13 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapControllers();
-
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000", "https://localhost:3000"));
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 
